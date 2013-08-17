0da6e531660fd6d774e21956f33e4e729e78f97b
Bartosz Gadomski <bartoszgadomski@users.noreply.github.com>
2013-08-17 20:31:28 +0200

    Create lang-pl.php

---
+++ php-upload/languages/lang-pl.php
@@ -0,0 +1,38 @@
+<?php
+
+   /**
+    *
+    * PHP Upload Class - polskie tłumaczenie
+    * 
+    * @author: Bartosz Gadomski <kontakt@bartoszgadomski.pl>
+    * @date: 2013-08-16
+    * @version: 0.1
+    * 
+    */  
+    
+    $_LANGUAGE = array( 'The upload path is invalid' => 'Ścieżka docelowa jest nieprawidłowa',
+                        'Uploaded file is not valid' => 'Wgrywany plik nie spełnia kryteriów walidacji',
+                        'There is no uploaded file' => 'Brak wgrywanego pliku',
+                        'The file data are incorrect' => 'Dane pliku są nieprawidłowe',
+                        'Upload error' => 'Błąd podczas wgrywania pliku',
+                        'The mime type is forbidden' => 'Typ MIME pliku jest nieakceptowany',
+                        'The file is too big' => 'Plik jest zbyt duży',
+                        'The uploaded file exceeds the upload_max_filesize directive in php.ini' => 'Wybrany plik jest większy niż dopuszcza ustawienie upload_max_filesize w php.ini',
+                        'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form' => 'Wybrany plik jest większy niż dopuszcza ustawienie MAX_FILE_SIZE w formularzu HTML',
+                        'The uploaded file was only partially uploaded' => 'Plik został przesłany tylko częściowo',
+                        'No file was uploaded' => 'Nie wybrano pliku do wgrania na serwer',
+                        'Missing a temporary folder' => 'Nie odnaleziono folderu tymczasowego',
+                        'Failed to write file to disk' => 'Błąd podczas zapisu pliku na dysk',
+                        'A PHP extension stopped the file upload' => 'PHP wstrzymał wgrywanie pliku',
+                        'Unknown size type' => 'Nieznany typ rozmiaru pliku',
+                        'Can not change file name - there is no uploaded file' => 'Nie można zmienić nazwy pliku - nie wybrano pliku do wgrania na serwer',
+                        'There is no file to upload' => 'Nie wybrano pliku do wgrania na serwer',
+                        'Can not move uploaded file - please try again later' => 'Nie można przenieść wgranego pliku do katalogu docelowego - proszę spróbować później' );    
+    
+   /**
+    *
+    * koniec pliku
+    * 
+    */                         
+
+?>
